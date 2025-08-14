ac1b8daffcec73ff8f4ad0410977e5fe57e1c3a8
BronteS003 <bronteslote@gmail.com>
2025-08-14 14:23:41 +0100

    Correct dates and model selection

--- model2_sterilization_status.R
+++ model2_sterilization_status.R
@@ -28,6 +28,16 @@ library(patchwork)
 
 sightings <- readRDS("sightings.rds", refhook = NULL)
 
+##CLEAN DATA##
+
+#Remove puppies as they're not relevant to analysis
+sightings <- sightings %>% 
+  filter(Puppy != 1)
+
+#Remove adults with unknown sterilization status 
+sightings <- sightings %>% 
+  filter(Unknown != 1)
+
 ##MODEL SELECTION##
 
 #Time Since Intervention
@@ -36,10 +46,21 @@ sightings <- readRDS("sightings.rds", refhook = NULL)
 m2_since <- glmer(Neutered ~ since_intervention + owned + subdistrict + sex +
                     (1 | polygon),
                   family = binomial, data = sightings, control = glmerControl(optimizer = "bobyqa"))
+#Check vif
+vif(m2_since) #all fine
+
+#Test which variable to drop
 drop1(m2_since, test = "Chisq")
 
+#Drop subdistrict
+m2_1since <- glmer(Neutered ~ since_intervention + owned + sex +
+                    (1 | polygon),
+                  family = binomial, data = sightings, control = glmerControl(optimizer = "bobyqa"))
+#Test which variable to drop
+drop1(m2_1since, test = "Chisq")#all variable significant
+
 #Final model for time since intervention
-m2_since_final<- glmer(Neutered ~ since_intervention + owned + subdistrict + sex +
+m2_since_final<- glmer(Neutered ~ since_intervention + owned + sex +
                          (1 | polygon),
                        family = binomial, data = sightings, control = glmerControl(optimizer = "bobyqa"))
 
@@ -49,38 +70,58 @@ m2_since_final<- glmer(Neutered ~ since_intervention + owned + subdistrict + sex
 m2_total <- glmer(Neutered ~ effort_humanpop + owned + subdistrict + sex +
                     (1 | polygon),
                   family = binomial, data = sightings, control = glmerControl(optimizer = "bobyqa"))
+#Check vif
+vif(m2_total)# all fine
+
+#Check which variables to drop
 drop1(m2_total, test = "Chisq")
 
-#Final model for total effort
-m2_total_final <- glmer(Neutered ~ effort_humanpop + owned + subdistrict + sex +
+#Drop subdistrict
+m2_1total <- glmer(Neutered ~ effort_humanpop + owned + sex +
                     (1 | polygon),
                   family = binomial, data = sightings, control = glmerControl(optimizer = "bobyqa"))
+#Check which variables to drop
+drop1(m2_1total, test = "Chisq")# all significant
+
+#Final model for total effort
+m2_total_final <- glmer(Neutered ~ effort_humanpop + owned + sex +
+                          (1 | polygon),
+                        family = binomial, data = sightings, control = glmerControl(optimizer = "bobyqa"))
 
 
 #Effort by year
 
 #Create first model using effort by year
-m2_year <- glmer(Neutered ~ last3y_humanpop + last2y_humanpop + last1y_humanpop + owned + subdistrict + sex +
+m2_year <- glmer(Neutered ~ effort_3y_humanpop + effort_2y_humanpop + effort_1y_humanpop + owned + subdistrict + sex +
                     (1 | polygon),
                   family = binomial, data = sightings, control = glmerControl(optimizer = "bobyqa"))
-drop1(m2_year, test = "Chisq")
-
-#Final model for effort by year
-m2_year_final <- glmer(Neutered ~ last3y_humanpop + last2y_humanpop + last1y_humanpop + owned + subdistrict + sex +
+vif(m2_year)#drop effort 2y
+m2_year <- glmer(Neutered ~ effort_3y_humanpop + effort_1y_humanpop + owned + subdistrict + sex +
                    (1 | polygon),
                  family = binomial, data = sightings, control = glmerControl(optimizer = "bobyqa"))
+vif(m2_year)#all fine now
+drop1(m2_year, test = "Chisq")
+
+#Drop subdistrict
+m2_1year <- glmer(Neutered ~ effort_3y_humanpop + effort_1y_humanpop + owned + sex +
+                   (1 | polygon),
+                 family = binomial, data = sightings, control = glmerControl(optimizer = "bobyqa"))
+drop1(m2_1year, test = "Chisq")
+
+#Final model for effort by year
+m2_year_final <- glmer(Neutered ~ effort_3y_humanpop + effort_1y_humanpop + owned + sex +
+                         (1 | polygon),
+                       family = binomial, data = sightings, control = glmerControl(optimizer = "bobyqa"))
 
 
 #Compare three sterilization effort indicators
-AIC(m2_since_final, m2_total_final, m2_year_final) #year model has the lowest AIC
+AIC(m2_since_final, m2_total_final, m2_year_final) #total effort has the lowest AIC
 
-#Conduct posthoc test
-emmeans(m2_year_final,pairwise ~ sex) #none of the interactions are significant, does this means we should drop sex?
 
 ##CHECK FOR OVERDISPERSION##
 
 #Visually check overdispersion using DHARMa plot
-simulationOutput_model2 <- simulateResiduals(fittedModel = m2_year_final) #create simulated data
+simulationOutput_model2 <- simulateResiduals(fittedModel = m2_total_final) #create simulated data
 testDispersion(simulationOutput_model2)
 
 #Test for outliers
@@ -93,104 +134,66 @@ testZeroInflation(simulationOutput_model2)
 
 ##PLOT MODELS##
 
-# Get predicted values over "owned" and "subdistrict"
-preds_owned <- ggpredict(m2_year_final, terms = c("owned", "subdistrict"))
+# Get predicted values over "owned"
+preds_owned <- ggpredict(m2_total_final, terms = c("owned"))
 
-# Plot Probability of Being Neutered by Sterilizations ownership status and subdistrict
+# Plot Probability of Being Neutered by Sterilizations ownership status 
 g1 <- ggplot(preds_owned, aes(x = x,
-                      y = predicted,
-                      fill = group)) +
-  geom_col(position = position_dodge(width = 0.5), width = 0.6) +
+                        y = predicted)) +
+  geom_col(width = 0.6, fill = "steelblue") +
   geom_errorbar(aes(ymin = conf.low,
                     ymax = conf.high),
-                position = position_dodge(width = 0.5),
                 width = 0.2) +
-  scale_x_discrete(labels = c("Yes", "No")) +
+  scale_x_discrete(labels = c("No", "Yes")) +
   labs(
-    title = "Predicted Probability of Being Neutered\nby Ownership Status and Subdistrict",
+    title = "Predicted Probability of Being Neutered\nby Ownership Status",
     x     = "Owned",
-    y     = "Predicted Probability",
-    fill  = "Subdistrict"
+    y     = "Predicted Probability"
   ) +
   theme_minimal(base_size = 14) +
   theme(
     plot.title = element_text(face = "bold", hjust = 0.5),
-    legend.position = "right",
+    legend.position = "none",
     axis.text = element_text(color = "gray30"),
-    panel.grid.minor = element_blank()) +
-  scale_color_viridis_d(option = "C", end = 0.9) +
-  scale_fill_viridis_d(option = "C", end = 0.9)
-
-
-
-# Get predicted values over "sex" and "subdistrict"
-preds_sex <- ggpredict(m2_year_final, terms = c("sex", "subdistrict"))
-
-# Plot Probability of Being Neutered by sex and subdistrict
-g2 <- ggplot(subset(preds_sex, group != "unknown"), aes(x = x,
-                                                  y = predicted,
-                                                  fill = group)) +
-  geom_col(position = position_dodge(width = 0.5), width = 0.6) +
-  geom_errorbar(aes(ymin = conf.low,
-                    ymax = conf.high),
-                position = position_dodge(width = 0.5),
-                width = 0.2) +
-  scale_x_discrete(labels = c("Female", "Male")) +
-  labs(
-    title = "Predicted Probability of Being Neutered\nby Sex and Subdistrict",
-    x     = "Sex",
-    y     = "Predicted Probability",
-    fill  = "Subdistrict"
-  ) +
-  theme_minimal(base_size = 14) +
-  theme(
-    plot.title = element_text(face = "bold", hjust = 0.5),
-    legend.position = "right",
-    axis.text = element_text(color = "gray30"),
-    panel.grid.minor = element_blank()) +
-  scale_color_viridis_d(option = "C", end = 0.9) +
-  scale_fill_viridis_d(option = "C", end = 0.9)
-
-#create final graph for sterilization by years
-
-# Reshape intervention by years
-sightings_long <- sightings %>%
-  pivot_longer(
-    cols = c(last3y_humanpop, last2y_humanpop, last1y_humanpop),
-    names_to = "YearBreakdown",
-    values_to = "sterilization_effort"
-  ) %>%
-  mutate(
-    YearBreakdown = case_when(
-      YearBreakdown == "last3y_humanpop" ~ 3,
-      YearBreakdown == "last2y_humanpop" ~ 2,
-      YearBreakdown == "last1y_humanpop" ~ 1
-    )
+    panel.grid.minor = element_blank()
   )
 
-#Refit model
-m2_final_long <- glmer(
-  Neutered ~ sterilization_effort * YearBreakdown + subdistrict + (1 | polygon),
-  family = binomial,
-  data = sightings_long,
-  control = glmerControl(optimizer = "bobyqa")
-)
 
-#Predicted values for m2_final_long over "YearBreakdown" and "subdistrict"
-preds2 <- ggpredict(m2_final_long, terms = c("YearBreakdown", "subdistrict"))
 
-# Plot
-g3 <- ggplot(preds2, aes(x = x, y = predicted, color = group, fill = group)) +
-  geom_line(linewidth = 1) +  
-  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.2, color = NA) +
-  scale_x_reverse(breaks = c(3, 2, 1)) +
+# Get predicted values over "sex"
+preds_sex <- ggpredict(m2_year_final, terms = c("sex"))
+
+# Plot Probability of Being Neutered by sex
+g2 <- ggplot(preds_sex, aes(x = x,
+                        y = predicted)) +
+  geom_col(width = 0.6, fill = "steelblue") +
+  geom_errorbar(aes(ymin = conf.low,
+                    ymax = conf.high),
+                width = 0.2) +
+  scale_x_discrete(labels = c("F", "M")) +
   labs(
-    title = "Predicted Probability of Being Neutered\nby Year",
-    x = "Years Ago",
-    y = "Probability of Being Neutered",
-    color = "Subdistrict",
-    fill = "Subdistrict"
+    title = "Predicted Probability of Being Neutered\nby Sex",
+    x     = "Sex",
+    y     = "Predicted Probability"
   ) +
+  theme_minimal(base_size = 14) +
+  theme(
+    plot.title = element_text(face = "bold", hjust = 0.5),
+    legend.position = "none",
+    axis.text = element_text(color = "gray30"),
+    panel.grid.minor = element_blank()
+  )
+
+#create final graph by total sterilization effort
+# Get predicted values over "effort_humanpop"
+preds_effort <- ggpredict(m2_total_final, terms = c("effort_humanpop"))
+
+# Plot Probability of Being Neutered by Sterilizations ownership status 
+g3 <- ggplot(preds_effort, aes(x = x, y = predicted)) +
+  stat_smooth(method = "lm") +
+  labs(title = "Probability of Being Neutered by\n Total Sterilization Effort",
+       x = "Total Sterilization Effort (Per Capita)",
+       y = "Probability of Being Neutered") +
   theme_minimal(base_size = 14) +
   theme(
     plot.title = element_text(face = "bold", hjust = 0.5),
@@ -199,6 +202,7 @@ g3 <- ggplot(preds2, aes(x = x, y = predicted, color = group, fill = group)) +
     panel.grid.minor = element_blank()) +
   scale_color_viridis_d(option = "C", end = 0.9) +
   scale_fill_viridis_d(option = "C", end = 0.9)
+
 
 #Combine into one panel 
 g1 + g2 + g3
