6b613581205e137cc0a144d67682d526c37fe035
BronteS003 <bronteslote@gmail.com>
2025-10-15 10:47:44 +0100

    Created updated sightings rds with full dataset
    Incomplete - still needs sterilization effort to be added

---
+++ FULL_sightings_RDS.R
@@ -0,0 +1,138 @@
+################################################################################
+##                      FULL Sightings RDS                                    ##
+################################################################################
+## Cleaned and organized sightings data set with the most up to date data as  ##
+## of October, 2025. For use in all future code using full data.              ##
+################################################################################
+## Created Oct. 15, 2025, by Bronte Slote, last updated Oct. 15               ##
+################################################################################
+
+##LOAD LIBRARIES##
+
+library(readr) #reading csv files
+library(dplyr) #organizing and manipulating data
+library(lubridate) #formatting dates and times
+library(stringr) #manipulating text
+
+
+##IMPORT AND CLEAN DATA##
+
+##Import Data set "sightings"
+sightings <- read.csv("FULL_sightings.csv")
+
+#Remove all rows that are resights
+sightings <- sightings %>%
+  filter(!str_to_lower(Notes) %in% c("resight", "unknown"))
+
+##Rename columns
+sightings <- sightings %>% rename("polygon" = "Sandbox.Name",#rename so easier to remember
+                                  "date"= "Timestamp")
+
+#Recognize polygon as a categorical variable with 7 levels
+sightings <- sightings %>%
+  mutate(polygon = factor (polygon, levels = c("Khok Kruat 01","Khok Kruat 06","Khok Kruat 07", "Tha Chang 12", "Tha Chang 16","Tha Chang 20","Tha Chang 24")))
+
+#Rename long form of polygon name to short form
+sightings <- sightings %>%
+  mutate(polygon = str_replace(polygon, "^Khok Kruat", "KK"),
+         polygon = str_replace(polygon, "^Tha Chang", "TC"))
+
+#Define column Neutered as numerical
+sightings <- sightings %>%
+  mutate(Neutered = as.numeric(Neutered)) #make "Neutered" numerical
+
+#Create subdstrict column
+sightings$subdistrict <- str_extract(sightings$polygon, "^[A-Za-z]+") #extract the letters from the "polygon" column and make them a new column "subdistrict"
+
+#Define subdistrict as categorical with 2 levels
+sightings <- sightings %>%
+  mutate(subdistrict = factor(subdistrict, levels = c("KK","TC"))) #make "subdistrict" a factor with the levels 'KK' and 'TC'
+
+#Convert date to a date variable
+sightings <- sightings %>%
+  mutate(date = substr(date, 1, 10))#just take date
+
+#Recognize date as date
+sightings <- sightings %>%
+  mutate(date = as.Date(date))
+
+#Create "since_intervention" column
+sightings <- sightings %>%
+  mutate(
+    intervention_start = case_when( #create column "intervention_start"
+      subdistrict == "KK" ~ as.Date("2022-02-11"), #where the subdistrict is "KK" make the intervention start date as 2022-02-11
+      subdistrict == "TC" ~ as.Date("2022-11-17") #where the subdistrict is "TC" make the intervention start date as 2022-11-17
+    ),
+    since_intervention = as.numeric(date - intervention_start) #create a new numeric column "since_intervention" by subtracting intervention start date from date of survey resulting ina column showing number of days since intervention
+  )
+
+#Make days since intervention to years
+sightings <- sightings %>%
+  mutate(since_intervention = since_intervention / 365)
+
+#Create survey identifier based on polygon and year of survey
+sightings <- sightings %>%
+  mutate(
+    year = format(as.Date(date), "%Y"),  # create a new column isolating year
+    survey = paste(polygon, year, sep = "_")  # create new column combining polygon and year to create a unique identifier for surveys
+  )
+
+#Make survey a factor variable  
+sightings <- sightings %>%
+  mutate(survey = as.factor(survey))
+
+#Create variable "day" to show either day 1 or 2 survey
+sightings <- sightings %>%
+  mutate(day = case_when( #create new column "day"
+    polygon == "KK 01" & date %in% as.Date(c("2021-10-07", "2023-03-01", "2024-08-27", "2025-09-04")) ~ 1,
+    polygon == "KK 06" & date %in% as.Date(c("2021-10-07", "2023-02-28", "2024-08-27", "2025-09-04")) ~ 1,
+    polygon == "KK 07" & date %in% as.Date(c("2021-10-07", "2023-02-28", "2024-08-27", "2025-09-04")) ~ 1,
+    polygon == "TC 12" & date %in% as.Date(c("2023-03-17", "2024-09-10", "2025-09-18")) ~ 1,
+    polygon == "TC 16" & date %in% as.Date(c("2023-03-17", "2024-09-10", "2025-09-10")) ~ 1,
+    polygon == "TC 20" & date %in% as.Date(c("2023-03-17", "2024-09-09", "2025-09-18")) ~ 1,
+    polygon == "Tc 24" & date %in% as.Date(c("2023-03-17", "2024-09-10", "2025-09-18")) ~ 1,
+    TRUE ~ 2  # everything else gets "2"
+  ))
+
+#Make day a factor variable  
+sightings <- sightings %>%
+  mutate(day= as.factor(day))
+
+#Make column age
+sightings <- sightings %>%
+  mutate(age = case_when(
+    Adult.male == 1 ~ "Adult",
+    Adult.NON.lactating.female == 1 | Adult.Lactating.female == 1 ~ "Adult",
+    Adult.unknown.sex == 1 ~ "Adult",
+    Puppy == 1 ~ "Puppy"))
+
+#Make age a factor
+sightings<-sightings %>%
+  mutate(age = as.factor(age))
+
+#Make column sex
+sightings <- sightings %>%
+  mutate(sex = case_when(
+    Adult.male == 1 ~ "M",
+    Adult.NON.lactating.female == 1 | Adult.Lactating.female == 1 ~ "F",
+    Adult.unknown.sex == 1|Puppy ==1 ~ "Unknown"))
+
+#Make sex a factor
+sightings<-sightings %>%
+  mutate(sex = as.factor(sex))
+
+#Make column owned
+sightings <- sightings %>%
+  mutate(owned = case_when(
+    Free.roaming.NO.collar == 1 ~ "No",
+    Free.roaming.collared | Confined.in.yard | On.chain.or.lead == 1 ~ "Yes"))
+
+#Make owned a factor
+sightings<-sightings %>%
+  mutate(owned = as.factor(owned))
+
+#Remove NAs
+sightings <- na.omit(sightings) #one guy that doesn't have ownership status which then causes the model to create error messages if you don't get rid of him
+
+
+##DEFINE STERILIZATION EFFORT##
